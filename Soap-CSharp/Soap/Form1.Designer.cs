36ff8dc4fcfd2bd0bb06e29705ec0b1d972c6baf
StoneMoe <lakechan96@gmail.com>
2016-01-08 02:55:00 +0800

    Added Fast-ODP Decrypt Part for Passengers

--- Soap-CSharp/Soap/Form1.Designer.cs
+++ Soap-CSharp/Soap/Form1.Designer.cs
@@ -28,73 +28,115 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabControl_Driver = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.button_fast_aes_gen = new System.Windows.Forms.Button();
-            this.textBox_fast_aes = new System.Windows.Forms.TextBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button_aes_gen = new System.Windows.Forms.Button();
+            this.textBox_aes = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox_fast_Content = new System.Windows.Forms.TextBox();
+            this.textBox_Content = new System.Windows.Forms.TextBox();
             this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_de_aes = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox_de_Content = new System.Windows.Forms.TextBox();
             this.textBox_odp_final = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.tabControl1.SuspendLayout();
+            this.label_tip = new System.Windows.Forms.Label();
+            this.tabControl_Driver.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // tabControl1
+            // tabControl_Driver
             // 
-            this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(12, 12);
-            this.tabControl1.Name = "tabControl1";
-            this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(449, 291);
-            this.tabControl1.TabIndex = 0;
+            this.tabControl_Driver.Controls.Add(this.tabPage1);
+            this.tabControl_Driver.Controls.Add(this.tabPage2);
+            this.tabControl_Driver.Location = new System.Drawing.Point(12, 12);
+            this.tabControl_Driver.Name = "tabControl_Driver";
+            this.tabControl_Driver.SelectedIndex = 0;
+            this.tabControl_Driver.Size = new System.Drawing.Size(449, 273);
+            this.tabControl_Driver.TabIndex = 0;
             // 
             // tabPage1
             // 
-            this.tabPage1.Controls.Add(this.button_fast_aes_gen);
-            this.tabPage1.Controls.Add(this.textBox_fast_aes);
+            this.tabPage1.Controls.Add(this.radioButton2);
+            this.tabPage1.Controls.Add(this.radioButton1);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.button_aes_gen);
+            this.tabPage1.Controls.Add(this.textBox_aes);
             this.tabPage1.Controls.Add(this.label4);
             this.tabPage1.Controls.Add(this.label3);
-            this.tabPage1.Controls.Add(this.textBox_fast_Content);
+            this.tabPage1.Controls.Add(this.textBox_Content);
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(441, 265);
+            this.tabPage1.Size = new System.Drawing.Size(441, 247);
             this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "Fast-ODP";
+            this.tabPage1.Text = "老司机模式";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
-            // button_fast_aes_gen
+            // radioButton2
             // 
-            this.button_fast_aes_gen.Location = new System.Drawing.Point(356, 231);
-            this.button_fast_aes_gen.Name = "button_fast_aes_gen";
-            this.button_fast_aes_gen.Size = new System.Drawing.Size(79, 21);
-            this.button_fast_aes_gen.TabIndex = 4;
-            this.button_fast_aes_gen.Text = "随机生成";
-            this.button_fast_aes_gen.UseVisualStyleBackColor = true;
-            this.button_fast_aes_gen.Click += new System.EventHandler(this.button_fast_aes_gen_Click);
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Enabled = false;
+            this.radioButton2.Location = new System.Drawing.Point(145, 219);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(155, 16);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.Text = "Classic-ODP (暂不支持)";
+            this.radioButton2.UseVisualStyleBackColor = true;
             // 
-            // textBox_fast_aes
+            // radioButton1
             // 
-            this.textBox_fast_aes.Location = new System.Drawing.Point(68, 231);
-            this.textBox_fast_aes.Name = "textBox_fast_aes";
-            this.textBox_fast_aes.Size = new System.Drawing.Size(282, 21);
-            this.textBox_fast_aes.TabIndex = 3;
-            this.textBox_fast_aes.TextChanged += new System.EventHandler(this.textBox_fast_aes_TextChanged);
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(68, 219);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(71, 16);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Fast-ODP";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 221);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "类型";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button_aes_gen
+            // 
+            this.button_aes_gen.Location = new System.Drawing.Point(356, 192);
+            this.button_aes_gen.Name = "button_aes_gen";
+            this.button_aes_gen.Size = new System.Drawing.Size(79, 21);
+            this.button_aes_gen.TabIndex = 4;
+            this.button_aes_gen.Text = "随机生成";
+            this.button_aes_gen.UseVisualStyleBackColor = true;
+            this.button_aes_gen.Click += new System.EventHandler(this.button_aes_gen_Click);
+            // 
+            // textBox_aes
+            // 
+            this.textBox_aes.Location = new System.Drawing.Point(67, 192);
+            this.textBox_aes.Name = "textBox_aes";
+            this.textBox_aes.Size = new System.Drawing.Size(283, 21);
+            this.textBox_aes.TabIndex = 3;
+            this.textBox_aes.TextChanged += new System.EventHandler(this.textBox_aes_TextChanged);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(9, 231);
+            this.label4.Location = new System.Drawing.Point(8, 195);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(53, 24);
+            this.label4.Size = new System.Drawing.Size(53, 12);
             this.label4.TabIndex = 2;
-            this.label4.Text = "消息密码\r\n(AES)";
+            this.label4.Text = "消息密码\r\n";
             this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label3
@@ -104,65 +146,99 @@
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(29, 36);
             this.label3.TabIndex = 1;
-            this.label3.Text = "消息\r\n\r\n内容";
+            this.label3.Text = "消息\r\n\r\n明文";
             // 
-            // textBox_fast_Content
+            // textBox_Content
             // 
-            this.textBox_fast_Content.Location = new System.Drawing.Point(67, 6);
-            this.textBox_fast_Content.Multiline = true;
-            this.textBox_fast_Content.Name = "textBox_fast_Content";
-            this.textBox_fast_Content.Size = new System.Drawing.Size(368, 219);
-            this.textBox_fast_Content.TabIndex = 0;
-            this.textBox_fast_Content.TextChanged += new System.EventHandler(this.textBox_fast_Content_TextChanged);
+            this.textBox_Content.Location = new System.Drawing.Point(67, 6);
+            this.textBox_Content.Multiline = true;
+            this.textBox_Content.Name = "textBox_Content";
+            this.textBox_Content.Size = new System.Drawing.Size(368, 180);
+            this.textBox_Content.TabIndex = 0;
+            this.textBox_Content.Text = "[将发车内容填写在这里，没有格式要求。]";
+            this.textBox_Content.TextChanged += new System.EventHandler(this.textBox_Content_TextChanged);
             // 
             // tabPage2
             // 
-            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.textBox_de_aes);
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.label6);
+            this.tabPage2.Controls.Add(this.textBox_de_Content);
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(441, 265);
+            this.tabPage2.Size = new System.Drawing.Size(441, 247);
             this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "Classic-ODP";
+            this.tabPage2.Text = "乘客模式";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
-            // label2
+            // textBox_de_aes
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(148, 118);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(155, 12);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "协议标准仍在拟定过程中...";
+            this.textBox_de_aes.Location = new System.Drawing.Point(67, 220);
+            this.textBox_de_aes.Name = "textBox_de_aes";
+            this.textBox_de_aes.Size = new System.Drawing.Size(368, 21);
+            this.textBox_de_aes.TabIndex = 7;
+            this.textBox_de_aes.TextChanged += new System.EventHandler(this.textBox_de_aes_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(8, 223);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 12);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "消息密码\r\n";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(19, 97);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 36);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "消息\r\n\r\n密文";
+            // 
+            // textBox_de_Content
+            // 
+            this.textBox_de_Content.Location = new System.Drawing.Point(67, 6);
+            this.textBox_de_Content.Multiline = true;
+            this.textBox_de_Content.Name = "textBox_de_Content";
+            this.textBox_de_Content.Size = new System.Drawing.Size(368, 208);
+            this.textBox_de_Content.TabIndex = 4;
+            this.textBox_de_Content.Text = "[请输入 ODP 标准消息，以 odp:// 开头，以 / 结尾。]";
+            this.textBox_de_Content.TextChanged += new System.EventHandler(this.textBox_de_Content_TextChanged);
             // 
             // textBox_odp_final
             // 
-            this.textBox_odp_final.Location = new System.Drawing.Point(12, 347);
+            this.textBox_odp_final.Location = new System.Drawing.Point(12, 325);
             this.textBox_odp_final.Multiline = true;
             this.textBox_odp_final.Name = "textBox_odp_final";
-            this.textBox_odp_final.Size = new System.Drawing.Size(445, 133);
+            this.textBox_odp_final.Size = new System.Drawing.Size(445, 155);
             this.textBox_odp_final.TabIndex = 1;
             // 
-            // label1
+            // label_tip
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(164, 320);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(149, 12);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "ODP 消息将自动生成在下方";
+            this.label_tip.AutoSize = true;
+            this.label_tip.Location = new System.Drawing.Point(175, 299);
+            this.label_tip.Name = "label_tip";
+            this.label_tip.Size = new System.Drawing.Size(125, 12);
+            this.label_tip.TabIndex = 2;
+            this.label_tip.Text = "结果将自动显示在下方";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(473, 492);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(470, 491);
+            this.Controls.Add(this.label_tip);
             this.Controls.Add(this.textBox_odp_final);
-            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.tabControl_Driver);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
-            this.Text = "Soap 0.1 | ODP 消息生成工具";
-            this.tabControl1.ResumeLayout(false);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Soap 0.1.1 | ODP 消息助手";
+            this.tabControl_Driver.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             this.tabPage1.PerformLayout();
             this.tabPage2.ResumeLayout(false);
@@ -174,17 +250,23 @@
 
         #endregion
 
-        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabControl tabControl_Driver;
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.TextBox textBox_odp_final;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button_fast_aes_gen;
-        private System.Windows.Forms.TextBox textBox_fast_aes;
+        private System.Windows.Forms.Label label_tip;
+        private System.Windows.Forms.Button button_aes_gen;
+        private System.Windows.Forms.TextBox textBox_aes;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox_fast_Content;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_Content;
+        private System.Windows.Forms.TextBox textBox_de_aes;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox_de_Content;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Soap-CSharp/Soap/Form1.cs
+++ Soap-CSharp/Soap/Form1.cs
@@ -9,36 +9,60 @@ namespace Soap
     {
         Random ra = new Random();
 
+        enum ODPMode
+        {
+            Fast = 0,
+            Classic = 1
+        }
+
         public Form1()
         {
             InitializeComponent();
         }
 
         #region Events
-        private void button_fast_aes_gen_Click(object sender, EventArgs e)
+        //Driver
+        private void button_aes_gen_Click(object sender, EventArgs e)
         {
-            textBox_fast_aes.Text = getRandomString(32);
+            textBox_aes.Text = getRandomString(32);
         }
-        private void textBox_fast_aes_TextChanged(object sender, EventArgs e)
+        private void textBox_aes_TextChanged(object sender, EventArgs e)
         {
-            gen_fast();
+            gen_en();
         }
-        private void textBox_fast_Content_TextChanged(object sender, EventArgs e)
+        private void textBox_Content_TextChanged(object sender, EventArgs e)
         {
-            gen_fast();
+            gen_en();
+        }
+        //Passenger
+        private void textBox_de_Content_TextChanged(object sender, EventArgs e)
+        {
+            gen_de();
+        }
+        private void textBox_de_aes_TextChanged(object sender, EventArgs e)
+        {
+            gen_de();
         }
         #endregion
 
-        #region ODPGenerator
-        private void gen_fast()
+        #region Generators
+        private void gen_en()
         {
-            string key = textBox_fast_aes.Text;
-            string content = textBox_fast_Content.Text;
-            textBox_odp_final.Text = encrypt_fast(content, key);
+            string key = textBox_aes.Text;
+            string content = textBox_Content.Text;
+            textBox_odp_final.Text = encrypt(ODPMode.Fast, content, key);
 
         }
-        private string encrypt_fast(string Content, string AesKey)
+        private void gen_de()
         {
+            string key = textBox_de_aes.Text;
+            string content = textBox_de_Content.Text;
+            textBox_odp_final.Text = decrypt(content, key);
+        }
+        private string encrypt(ODPMode mode, string Content, string AesKey)
+        {
+            byte[] FinalArray;
+
             //处理AES Key
             string key = AesKey;
 
@@ -55,32 +79,108 @@ namespace Soap
             string wait4AES = "local@" + ContentBase64;
 
             //3. AES 加密
-            byte[] keyArray = UTF8Encoding.UTF8.GetBytes(key);
+            try
+            {
+                byte[] keyArray = UTF8Encoding.UTF8.GetBytes(key);
 
-            RijndaelManaged rDel = new RijndaelManaged();
-            rDel.Key = keyArray;
-            rDel.Mode = CipherMode.ECB; //使用 ECB Mode
-            rDel.Padding = PaddingMode.PKCS7; //Padding Mode PKCS7
-            ICryptoTransform cTransform = rDel.CreateEncryptor();
+                RijndaelManaged rDel = new RijndaelManaged();
+                rDel.Key = keyArray;
+                rDel.Mode = CipherMode.ECB; //使用 ECB Mode
+                rDel.Padding = PaddingMode.PKCS7; //Padding Mode PKCS7
+                ICryptoTransform cTransform = rDel.CreateEncryptor();
 
-            byte[] ContentBase64Array = UTF8Encoding.UTF8.GetBytes(ContentBase64);
-            byte[] FinalArray = cTransform.TransformFinalBlock(ContentBase64Array, 0, ContentBase64Array.Length);
+                byte[] wait4AESArray = UTF8Encoding.UTF8.GetBytes(wait4AES);
+                FinalArray = cTransform.TransformFinalBlock(wait4AESArray, 0, wait4AESArray.Length);
+            }
+            catch (Exception)
+            {
+                return "消息密码 (AES Key) 仅允许使用大小写英文，数字，与半角标点符号";
+            }
 
             //4. 对 AES 加密后内容再次进行 Base64 编码，保证为可见字符
             string FinalStr = Convert.ToBase64String(FinalArray, 0, FinalArray.Length);
 
             //5. 拼接 Url Scheme
             return "odp://" + FinalStr + "/";
+
+
+        }
+        private string decrypt(string Content, string AesKey)
+        {
+            byte[] resultArray;
+
+            //检查 ODP 消息格式
+            if (Content.Length <= 6)
+            {
+                return "等待输入";
+            }
+            if (Content.Substring(0, 6).ToLower() != "odp://")
+            {
+                return "不是标准的 ODP 消息体";
+            }
+            if (Content[Content.Length - 1] != '/')
+            {
+                return "ODP 消息结尾需要 '/' ";
+            }
+
+            //处理AES Key
+            string key = AesKey;
+
+            //不足32时使用odp三个字符循环直到补全为止
+            string paddingStr = "odpodpodpodpodpodpodpodpodpodpodp";
+            key += paddingStr;
+            key = key.Substring(0, 32);
+
+            //1. 提取密文
+            string rawContent = Content.Substring(6, Content.Length - 7);
+
+            //2. Base64 解码
+            byte[] AESEncryptArray = Convert.FromBase64String(rawContent);
+
+            //3. AES 解密
+            try
+            {
+                byte[] keyArray = UTF8Encoding.UTF8.GetBytes(key);
+
+                RijndaelManaged rDel = new RijndaelManaged();
+                rDel.Key = keyArray;
+                rDel.Mode = CipherMode.ECB;
+                rDel.Padding = PaddingMode.PKCS7;
+
+                ICryptoTransform cTransform = rDel.CreateDecryptor();
+                resultArray = cTransform.TransformFinalBlock(AESEncryptArray, 0, AESEncryptArray.Length);
+            }
+            catch (Exception)
+            {
+                return "密码与消息可能不匹配";
+            }
+
+            //4. 判断类型
+            string ContentBase64withTag = UTF8Encoding.UTF8.GetString(resultArray);
+
+            if (ContentBase64withTag.ToLower().Contains("local@"))
+            {
+                string ContentBase64 = ContentBase64withTag.Replace("local@", "");
+                return UTF8Encoding.UTF8.GetString(Convert.FromBase64String(ContentBase64));
+            }
+            else if (ContentBase64withTag.ToLower().Contains("remote@"))
+            {
+                return "Classic-ODP 暂不支持";
+            }
+            else
+            {
+                return "未知的 ODP 类型，Soap 无法解析该消息";
+            }
         }
         #endregion
 
         private string getRandomString(int _Length)
         {
-            string _strList = "qwertyuioplkjhgfdsazxcvbnm1234567890";
+            string _strList = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuioplkjhgfdsazxcvbnm1234567890";
             string _buffer = "";
             for (int i = 1; i <= _Length; i++)
             {
-                _buffer += _strList[ra.Next(0, 35)];
+                _buffer += _strList[ra.Next(0, 61)];
             }
             return _buffer;
         }
